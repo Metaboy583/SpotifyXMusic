5b1f0b019ad7cc961153cc90fb74f9675f65481a
Metaboy583 <123310356+Metaboy583@users.noreply.github.com>
2023-03-09 12:36:25 +0530

    Update Youtube.py

--- SpotifyX/platforms/Youtube.py
+++ SpotifyX/platforms/Youtube.py
@@ -9,8 +9,8 @@ from pyrogram.types import Message
 from youtubesearchpython.__future__ import VideosSearch
 
 import config
-from AnonX.utils.database import is_on_off
-from AnonX.utils.formatters import time_to_seconds
+from SpotifyX.utils.database import is_on_off
+from SpotifyX.utils.formatters import time_to_seconds
 
 
 async def shell_cmd(cmd):
