3fec2ed2c78968907eb6cbeb1ca55443de80ee55
Metaboy583 <123310356+Metaboy583@users.noreply.github.com>
2023-03-09 11:29:43 +0530

    Create __init__.py

---
+++ SpotifyX/__init__.py
@@ -0,0 +1,37 @@
+from SpotifyX.core.bot import AnonXBot
+from SpotifyX.core.dir import dirr
+from SpotifyX.core.git import git
+from SpotifyX.core.userbot import Userbot
+from SpotifyX.misc import dbb, heroku, sudo
+from aiohttp import ClientSession
+
+from .logging import LOGGER
+
+
+dirr()
+
+git()
+
+dbb()
+
+heroku()
+
+sudo()
+
+# Clients
+app = SpotifyXBot()
+
+userbot = Userbot()
+
+
+from .platforms import *
+
+YouTube = YouTubeAPI()
+Carbon = CarbonAPI()
+Spotify = SpotifyAPI()
+Apple = AppleAPI()
+Resso = RessoAPI()
+SoundCloud = SoundAPI()
+Telegram = TeleAPI()
+
+aiohttpsession = ClientSession()
