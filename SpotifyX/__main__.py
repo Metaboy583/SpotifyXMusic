fec325aa466b4af664949a99d782269c0fd327b2
Metaboy583 <123310356+Metaboy583@users.noreply.github.com>
2023-03-09 11:54:54 +0530

    Create __main__.py

---
+++ SpotifyX/__main__.py
@@ -0,0 +1,76 @@
+import asyncio
+import importlib
+import sys
+
+from pyrogram import idle
+from pytgcalls.exceptions import NoActiveGroupCall
+
+import config
+from config import BANNED_USERS
+from SpotifyX import LOGGER, app, userbot
+from SpotifyX.core.call import Anon
+from SpotifyX.plugins import ALL_MODULES
+from SpotifyX.utils.database import get_banned_users, get_gbanned
+
+loop = asyncio.get_event_loop()
+
+
+async def init():
+    if (
+        not config.STRING1
+        and not config.STRING2
+        and not config.STRING3
+        and not config.STRING4
+        and not config.STRING5
+    ):
+        LOGGER("SpotifyX").error(
+            "WTF Baby ! Atleast add a pyrogram string, How Cheap..."
+        )
+        return
+    if (
+        not config.SPOTIFY_CLIENT_ID
+        and not config.SPOTIFY_CLIENT_SECRET
+    ):
+        LOGGER("SpotifyX").warning(
+            "Sur spotify id aur secret toh daala hi nahi aapne ab toh spotify se nahi chala paaoge gaane."
+        )
+    try:
+        users = await get_gbanned()
+        for user_id in users:
+            BANNED_USERS.add(user_id)
+        users = await get_banned_users()
+        for user_id in users:
+            BANNED_USERS.add(user_id)
+    except:
+        pass
+    await app.start()
+    for all_module in ALL_MODULES:
+        importlib.import_module("SpotifyX.plugins." + all_module)
+    LOGGER("SpotifyX.plugins").info(
+        "Necessary Modules Imported Successfully."
+    )
+    await userbot.start()
+    await Spotify.start()
+    try:
+        await Spotify.stream_decall("https://te.legra.ph/file/3f6a4f4797027a9a8b2b2.mp4")
+    except:
+        pass
+    try:
+        await Spotify.stream_call(
+            "https://te.legra.ph/file/3f6a4f4797027a9a8b2b2.mp4"
+        )
+    except NoActiveGroupCall:
+        LOGGER("SpotifyX").error(
+            "[ERROR] - \n\nHey Baby, firstly open telegram and turn on voice chat in Logger Group else fu*k off. If you ever ended voice chat in log group i will stop working and users will fu*k you up."
+        )
+        sys.exit()
+    except:
+        pass
+    await Spotify.decorators()
+    LOGGER("SpotifyX").info("\x41\x6e\x6f\x6e\x58\x20\x4d\x75\x73\x69\x63\x20\x42\x6f\x74\x20\x53\x74\x61\x72\x74\x65\x64\x20\x53\x75\x63\x63\x65\x73\x73\x66\x75\x6c\x6c\x79\x2e\x2e\x2e\n\n\x4e\x6f\x77\x20\x64\x72\x6f\x70\x20\x79\x6f\x75\x72\x20\x67\x69\x72\x6c\x66\x72\x69\x65\x6e\x64\'\x73\x20\x6e\x75\x64\x65\x73\x20\x61\x74\x20\x40\x44\x65\x76\x69\x6c\x73\x48\x65\x61\x76\x65\x6e\x4d\x46")
+    await idle()
+
+
+if __name__ == "__main__":
+    loop.run_until_complete(init())
+    LOGGER("SpotifyX").info("Stopping Music Bot...")
