df253045ae07bf28d5508c2ebf79d06096c710a7
Metaboy583 <123310356+Metaboy583@users.noreply.github.com>
2023-03-09 17:39:47 +0530

    Update thumbnails.py

--- SpotifyX/utils/thumbnails.py
+++ SpotifyX/utils/thumbnails.py
@@ -82,7 +82,7 @@ async def gen_thumb(videoid, user_id):
         x = f.resize((107, 107))
 
         youtube = Image.open(f"cache/thumb{videoid}.png")
-        bg = Image.open(f"SpotifyX/assets/anonx.png")
+        bg = Image.open(f"SpotifyX/assets/spotifyx.png")
         image1 = changeImageSize(1280, 720, youtube)
         image2 = image1.convert("RGBA")
         background = image2.filter(filter=ImageFilter.BoxBlur(30))
@@ -259,7 +259,7 @@ async def gen_qthumb(videoid, user_id):
         draw = ImageDraw.Draw(background)
         font = ImageFont.truetype("SpotifyX/assets/font2.ttf", 45)
         ImageFont.truetype("SpotifyX/assets/font2.ttf", 70)
-        arial = ImageFont.truetype("AnonX/assets/font2.ttf", 30)
+        arial = ImageFont.truetype("SpotifyX/assets/font2.ttf", 30)
         ImageFont.truetype("SpotifyX/assets/font.ttf", 30)
         para = textwrap.wrap(title, width=32)
         try:
